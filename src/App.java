d33eadc3b26083d8a58d09e64ae0d98f4c70fad2
Bayron150 <lolitoelol@gmail.com>
2025-08-19 18:58:40 -0500

    Se modificaron los cambios

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
     System.out.println("Hello, World!");
 
     // Create an instance of Persona
-    Persona persona1 = new Persona("Juan", "Perez", 30, "juan@gmail.com");
+    Persona persona1 = new Persona("Zuzana", "Horia", 30, "zhoria@gmail.com");
 
     Persona persona2 = new Persona("Maria", "Gomez");
 
