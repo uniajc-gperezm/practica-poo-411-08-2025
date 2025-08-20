78284c6056cc51ce0b401b155c1b8f7200a2730a
Giubran Mideros <gmideros@estudiante.uniajc.edu.co>
2025-08-19 19:21:27 -0500

    Atrubito trabajo

--- src/Persona.java
+++ src/Persona.java
@@ -17,7 +17,7 @@ public class Persona {
   }
   
   
-  public Persona(String nombre, String apellido, int edad, String correo) {
+  public Persona(String nombre, String apellido, int edad, String correo, String trabajo) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
@@ -30,6 +30,7 @@ public class Persona {
     System.out.println("Apellido: " + apellido);
     System.out.println("Edad: " + edad);
     System.out.println("Correo: " + correo);
+    System.out.println("Trabajo: " + trabajo);
   }
 
 }
