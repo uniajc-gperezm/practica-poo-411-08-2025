c7b15de9ff470afdd6b623051a0a17397ab77fb3
Estevan Mejia <jestevanmejia@estudiante.uniajc.edu.co>
2025-08-19 19:09:40 -0500

    Agregué atributo nuevo a la clase Estudiante

---
+++ src/Estudiante.java
@@ -0,0 +1,42 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+
+    private String correo;
+
+    public Estudiante(String nombre, int edad, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void imprimir() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Correo: " + correo);
+    }
+}
